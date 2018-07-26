eaf1f6ad87132a2c3db04e8c376b11c8ea34dc44
mashihara <mashiharahiroyuki@gmail.com>
2018-07-26 19:28:46 +0900

    feat(Navigation):NavigationをMainに戻した

--- src/components/parts/common/Main.vue
+++ src/components/parts/common/Main.vue
@@ -1,10 +1,43 @@
 <template>
   <v-app id="inspire">
-    <Navigation />
     <!-- navigation -->
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      clipped
+      class="grey lighten-5"
+      app
+    >
+      <v-list
+        class="grey lighten-5 pt-5"
+      >
+        <template v-for="(item, i) in items">
+          <v-divider
+            v-if="item.divider"
+            :key="i"
+            class="my-3"
+          ></v-divider>
+          <v-list-tile
+            v-else
+            :key="i"
+            @click="pushLink(item.link)"
+          >
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title class="subheading grey--text text--darken-4">
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
     <!-- header -->
     <v-toolbar color="accent" app absolute clipped-left>
-      <v-toolbar-side-icon @click="changeNavDrawer"></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click.native="drawer = !drawer"></v-toolbar-side-icon>
       <span class="teal--text text--darken-2 headline ml-3 mr-5 wf-nicomoji" @click="titlePush">
         テニスベア
       </span>
@@ -19,18 +52,29 @@
 </template>
 
 <script>
-import Navigation from '@/components/parts/common/Navigation'
 export default {
-  components: {Navigation},
+  data: () => ({
+    drawer: null,
+    items: [
+      {icon: 'home', text: 'ホーム', link: '/'},
+      {divider: true},
+      {icon: 'map', text: 'テニスコートを探す', link: '/tokyo-tennis-map'},
+      {icon: 'people', text: 'テニスサークルを探す', link: '/tokyo-tennis-map'},
+      {icon: 'directions_run', text: 'テニススクールを探す', link: '/tokyo-tennis-court'},
+      {divider: true},
+      {icon: 'touch_app', text: 'about us', link: '/tokyo-tennis-court'},
+      {divider: true},
+      {icon: 'fingerprint', text: 'ログイン', link: '/tokyo-tennis-court'},
+      {divider: true},
+      {icon: 'touch_app', text: '東京壁打ちマップ'}
+    ]
+  }),
   methods: {
-    pushLink: function () {
-      this.$router.push('/placeEdit')
+    pushLink: function (link) {
+      this.$router.push(link)
     },
     titlePush: function () {
       this.$router.push('/')
-    },
-    changeNavDrawer: function () {
-      this.$store.commit('changeNavDrawer')
     }
   }
 }
@@ -41,6 +85,9 @@ export default {
   }
   .text {
     font-weight: 400;
+  }
+  .v-navigation-drawer__border {
+    display: none;
   }
   .wf-nicomoji {
     font-family: "Nico Moji"!important;
--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -1,11 +1,7 @@
 const common = {
   state: {
-    navDrawer: false
   },
   mutations: {
-    changeNavDrawer: (state) => {
-      state.navDrawer = !state.navDrawer
-    }
   }
 }
 export default common
