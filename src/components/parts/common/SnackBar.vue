6c6d34c08f29ef884267a208ae923fe3bf5eeb51
mashihara <mashiharahiroyuki@gmail.com>
2018-08-03 09:50:57 +0900

    fix(snackbar):スナックバーを追加

---
+++ src/components/parts/common/SnackBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-snackbar
+    :value="flg"
+    :timeout="6000"
+    bottom
+    left
+  >
+    {{ text }}
+    <v-btn
+      color="pink"
+      flat
+      @click="$emit('changeSnackBarFlg',false)"
+    >
+      Close
+    </v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  name: 'SnackBar',
+  props: ['snackbarFlg', 'text'],
+  computed: {
+    flg: {
+      get () {
+        console.log('get')
+        return this.snackbarFlg
+      },
+      set () {
+        console.log('set')
+        this.$emit('changeSnackBarFlg')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
